8a08d8d0264c6305b4be1f1e376793ee95488e2e
alisson.santos <alisson.santos@cwi.com.br>
2026-03-31 19:52:02 -0300

    Initial commit

---
+++ src/main/java/br/com/sicred/assemblyvote/AssemblyVoteApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicred.assemblyvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyVoteApplication.class, args);
+	}
+
+}
